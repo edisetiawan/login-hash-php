e5bb81e4a3be1bbf819868591232116fec2c5644
Edi Setiawan <edisetiawan@users.noreply.github.com>
2015-12-08 23:18:54 +0700

    Create login.php

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <form method="post" action="login_check.php">
 Username : <input type="text" name="username"><br>
-password : <input type="text" name="password"><br>
+password : <input type="password" name="password"><br>
 <input type="submit" name="login" value="login">
 </form>
